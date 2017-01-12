d2003b6758f7787fce0ea2050da26996bff11027
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2017-01-12 16:13:56 +0100

    Two pane view for different interpolation algorithms

--- source/Application.cpp
+++ source/Application.cpp
@@ -32,9 +32,14 @@ void Application::onAnimationRequest(
 )
 {
     _inAnimationMode = true;
-    _leftAnimator = std::make_shared<PumaStraightLineAnimator>();
+
+    _leftAnimator = std::make_shared<PumaFastAnimator>();
     _leftAnimator->setTarget(_pumaCalculator);
     _leftAnimator->startAnimation(start, end);
+
+    _rightAnimator = std::make_shared<PumaStraightLineAnimator>();
+    _rightAnimator->setTarget(_alternativePumaCalculator);
+    _rightAnimator->startAnimation(start, end);
 }
 
 void Application::onCreate()
@@ -91,6 +96,7 @@ void Application::onUpdate(
     {
         std::chrono::duration<double> deltaSeconds{deltaTime};
         _leftAnimator->update(deltaSeconds.count());
+        _rightAnimator->update(deltaSeconds.count());
 
         showAnimationInferface();
     }
@@ -120,7 +126,7 @@ void Application::onRender()
         render(*_pumaModel.get());
 
         glViewport(resolution.x/2, 0, resolution.x/2, resolution.y);
-        render(*_pumaModel.get());
+        render(*_alternativePumaModel.get());
     }
 
     ImGuiApplication::onRender();
@@ -258,6 +264,7 @@ void Application::showAnimationInferface()
     if (ImGui::Button("Play"))
     {
         _leftAnimator->play();
+        _rightAnimator->play();
     }
 
     ImGui::SameLine();
@@ -265,6 +272,7 @@ void Application::showAnimationInferface()
     if (ImGui::Button("Pause"))
     {
         _leftAnimator->pause();
+        _rightAnimator->pause();
     }
 
     ImGui::SameLine();
@@ -272,11 +280,13 @@ void Application::showAnimationInferface()
     if (ImGui::Button("Restart"))
     {
         _leftAnimator->restart();
+        _rightAnimator->restart();
     }
 
     float animationDuration = static_cast<float>(_leftAnimator->getDuration());
     ImGui::SliderFloat("Duration", &animationDuration, 0.01f, 100.0f);
     _leftAnimator->setDuration(animationDuration);
+    _rightAnimator->setDuration(animationDuration);
 
     float fraction = static_cast<float>(
         _leftAnimator->getElapsed() / _leftAnimator->getDuration()
@@ -288,6 +298,7 @@ void Application::showAnimationInferface()
     {
         _inAnimationMode = false;
         _leftAnimator = nullptr;
+        _rightAnimator = nullptr;
     }
 
     ImGui::End();
